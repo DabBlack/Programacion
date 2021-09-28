4b5c591fd835a09e16f92469853d972d34aac6d2
david <david@DESKTOP-QJ0G38O>
2021-09-28 18:59:20 +0200

    Actualizacion Ejercicios

--- Primer Trimestre/Proyectos/EjerciciosProgramación/src/Bloque01/Ejercicio01.java
+++ Primer Trimestre/Proyectos/EjerciciosProgramación/src/Bloque01/Ejercicio01.java
@@ -5,16 +5,16 @@ import javax.swing.JOptionPane;
 public class Ejercicio01 {
 
 	public static void main(String[] args) {
-		String str = JOptionPane.showInputDialog("Introduzca un número entero: ");
-		int var1 = Integer.parseInt(str); //Pido un número entero al usuario
+		String str = JOptionPane.showInputDialog("Introduzca un numero entero: ");
+		int var1 = Integer.parseInt(str); //Pido un numero entero al usuario
 			
-		str = JOptionPane.showInputDialog("Introduzca un número flotante: ");
-		float var2 = Float.parseFloat(str); //Pido un número flotante al usuario
+		str = JOptionPane.showInputDialog("Introduzca un numero flotante: ");
+		float var2 = Float.parseFloat(str); //Pido un numerp flotante al usuario
 			
-		str = JOptionPane.showInputDialog("Introduzca un número doble: ");
-		double var3 = Double.parseDouble(str); //Pido un número doble al usuario
+		str = JOptionPane.showInputDialog("Introduzca un numero doble: ");
+		double var3 = Double.parseDouble(str); //Pido un numero doble al usuario
 			
-		System.out.println("Número entero: " + var1 + " Número flotante: " + var2 + " Número doble: " + var3);
-		//Muestro consecutivamente los números introducidos por el usuario
+		System.out.println("Numero entero: " + var1 + " Numero flotante: " + var2 + " Numero doble: " + var3);
+		//Muestro consecutivamente los numero introducidos por el usuario
 	}
 }
