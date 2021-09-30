c547da4966a99aaa9e214bc26399d999970c032c
DabBlack <davidalgar2@gmail.com>
2021-09-30 14:17:08 +0200

    Actualizacion Ejercicios

---
+++ Primer Trimestre/Proyectos/EjerciciosProgramación/src/bloque01_cap01/Ejercicio05.java
@@ -0,0 +1,25 @@
+package bloque01_cap01;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio05 {
+
+	public static void main(String[] args) {
+
+		String str = JOptionPane.showInputDialog("Introduzca un numero: "); //Declaramos la variable string
+		int var1 = Integer.parseInt(str); //Declaro el primer numero entero
+		
+		str = JOptionPane.showInputDialog("Introduzca un numero: "); 
+		int var2 = Integer.parseInt(str); //Declaro el primer numero entero
+		
+		System.out.println("Numeros antes del cambio: " + var1 + ", " + var2); 
+		//Mostramos en pantalla las variables var1 y var2
+		
+		int var3 = var1; //Declaamos la variable var3 de tipo int y le asignamos el valor que tiene var1
+		var1 = var2; //Le asignamos a la variable var1 el valor que tiene var2
+		var2 = var3; //Le asignamos a la variable var2 el valor que tiene var3
+		
+		System.out.println("Numeros despues del cambio: " + var1 + ", "  + var2);
+		//Mostramos en pantalla las variables var1 y var2 después del intercambio
+	}
+}
