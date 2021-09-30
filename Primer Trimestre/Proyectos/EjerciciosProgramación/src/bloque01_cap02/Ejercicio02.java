906c3c269d0c69a1ee85c9e5d182ba7ecfd50bea
DabBlack <davidalgar2@gmail.com>
2021-09-30 14:15:33 +0200

    Actualizacion Ejercicios

---
+++ Primer Trimestre/Proyectos/EjerciciosProgramación/src/bloque01_cap02/Ejercicio02.java
@@ -0,0 +1,31 @@
+package bloque01_cap02;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = JOptionPane.showInputDialog("Introduzca un numero: "); //Se declara la variable str de tipo
+		//String y se pide al usuario que introduzca un valor
+		int var1 = Integer.parseInt(str);
+		System.out.println("Primer numero introducido: " + var1); //Se muesra en pantalla el primer valor introducido
+		
+		str = JOptionPane.showInputDialog("Introduzca un segundo numero: "); //Se pide al usuario que introduzca un valor
+		int var2 = Integer.parseInt(str);
+		System.out.println("Segundo numero introducido: " + var2); //Se muesra en pantalla el segundo valor introducido
+		
+		if (var1 < var2) {
+			System.out.println("Numero menor: " + var1);
+		}
+	
+		else {
+			if (var2 < var1) {
+				System.out.println("Numero menor: " + var2);
+			}
+			
+		}
+		
+	}
+
+}
