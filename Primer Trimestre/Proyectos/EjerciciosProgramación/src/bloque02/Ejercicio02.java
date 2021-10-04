bc9af440a97e29f889e09d7ff030c11a58055381
DabBlack <davidalgar2@gmail.com>
2021-10-04 13:07:44 +0200

    Actualizacion Ejercicios

--- Primer Trimestre/Proyectos/EjerciciosProgramación/src/bloque02/Ejercicio02.java
+++ Primer Trimestre/Proyectos/EjerciciosProgramación/src/bloque02/Ejercicio02.java
@@ -2,146 +2,134 @@ package bloque02;
 
 import javax.swing.JOptionPane;
 
-public class Ejercicio01 {
+public class Ejercicio02 {
 
 	public static void main(String[] args) {
 		
 		String str = JOptionPane.showInputDialog("Introduzca un primer numero: "); //Se declara la variable str de
 		//tipo String y se pide al usuario que introduzca un valor
 		int var1 = Integer.parseInt(str); //Se declara la variable var1 de tipo int
+		if (var1 < 0) {
+			System.out.println("La aplicacion no admite numeros negativos.");
+			return;
+		} //Si la varible es negativa se detiene el programa
 		
 		str = JOptionPane.showInputDialog("Introduzca un segundo numero: "); //Se pide al usuario que introduzca un valor
 		int var2 = Integer.parseInt(str); //Se declara la variable var2 de tipo int
+		if (var2 < 0) {
+			System.out.println("La aplicacion no admite numeros negativos.");
+			return;
+		} //Si la varible es negativa se detiene el programa
 		
 		str = JOptionPane.showInputDialog("Introduzca un tercer numero: "); //Se pide al usuario que introduzca un valor
 		int var3 = Integer.parseInt(str); //Se declara la variable var3 de tipo int
+		if (var3 < 0) {
+			System.out.println("La aplicacion no admite numeros negativos.");
+			return;
+		} //Si la varible es negativa se detiene el programa
 		
 		str = JOptionPane.showInputDialog("Introduzca cuarto un numero: "); //Se pide al usuario que introduzca un valor
 		int var4 = Integer.parseInt(str); //Se declara la variable var4 de tipo int
+		if (var4 < 0) {
+			System.out.println("La aplicacion no admite numeros negativos.");
+			return;
+		} //Si la varible es negativa se detiene el programa
 		
 		str = JOptionPane.showInputDialog("Introduzca quinto un numero: "); //Se pide al usuario que introduzca un valor
-		int var5 = Integer.parseInt(str); //Se declara la variable var5 de tipo int
+		int var5 = Integer.parseInt(str); //Se declara la variable var4 de tipo int
+		if (var5 < 0) {
+			System.out.println("La aplicacion no admite numeros negativos.");
+			return;
+		} //Si la varible es negativa se detiene el programa
 		
 		System.out.println("Primer valor: " + var1 + "\nSegundo valor: " + var2 + "\nTercer valor: " + var3 +
 				"\nCuarto valor: " + var4 + "\nQuinto valor: " + var5);
 		
-		int negativos = 0;
 		int bajos = 0;
 		int medios = 0;
 		int grandes = 0;
 		
 		//Primer numero
-		
-		if (var1 < 0) {
-			negativos = negativos + var1;
-		}
-			
-			else {
-				if (var1 >= 0 && var1 <=25) {
-					bajos = bajos + var1;
+				
+		if (var1 >= 0 && var1 <=25) {
+			bajos = bajos + 1;
 			}
 				
 				else {
 					if (var1 >= 26 && var1 <=250) {
-						medios = medios + var1;
+						medios = medios + 1;
 				}
 					
 					else {
-						grandes = grandes + var1;
+						grandes = grandes + 1;
 					}
-				}
-			}
+		}
 		
 		//Segundo numero
 		
-		if (var2 < 0) {
-			negativos = negativos + var2;
-		}
-					
-			else {
-				if (var2 >= 0 && var2 <=25) {
-					bajos = bajos + var2;
+		if (var2 >= 0 && var2 <=25) {
+			bajos = bajos + 1;
 			}
 						
 				else {
 					if (var2 >= 26 && var2 <=250) {
-						medios = medios + var2;
+						medios = medios + 1;
 				}
 							
 					else {
-						grandes = grandes + var2;
+						grandes = grandes + 1;
 					}
 				}
-			}
 
 		//Tercer numero
 		
-		if (var3 < 0) {
-			negativos = negativos + var3;
-		}
-					
-			else {
-				if (var3 >= 0 && var3 <=25) {
-					bajos = bajos + var3;
+		if (var3 >= 0 && var3 <=25) {
+			bajos = bajos + 1;
 			}
 						
 				else {
 					if (var3 >= 26 && var3 <=250) {
-						medios = medios + var3;
+						medios = medios + 1;
 				}
 							
 					else {
-						grandes = grandes + var3;
+						grandes = grandes + 1;
 					}
 				}
-			}
-
+		
 		//Cuarto numero
 		
-		if (var4 < 0) {
-			negativos = negativos + var4;
-		}
-					
-			else {
-				if (var4 >= 0 && var4 <=25) {
-					bajos = bajos + var4;
+		if (var4 >= 0 && var4 <=25) {
+			bajos = bajos + 1;
 			}
 				
 				else {
 					if (var4 >= 26 && var4 <=250) {
-						medios = medios + var4;
+						medios = medios + 1;
 				}
 							
 					else {
-						grandes = grandes + var4;
+						grandes = grandes + 1;
 					}
 				}
-			}
 
 		//Quinto numero
 		
-		if (var5 < 0) {
-			negativos = negativos + var5;
-		}
-					
-			else {
-				if (var5 >= 0 && var5 <=25) {
-					bajos = bajos + var5;
+		if (var5 >= 0 && var5 <=25) {
+			bajos = bajos + 1;
 			}
 						
 				else {
 					if (var5 >= 26 && var5 <=250) {
-						medios = medios + var5;
+						medios = medios + 1;
 				}
 							
 					else {
-						grandes = grandes + var5;
+						grandes = grandes + 1;
 					}
 				}
-			}
 		
-		System.out.println("Suma de los numeros negativos: " + negativos + "\nSuma de los numeros bajos: " + bajos + "\nSuma de los numeros medios: "
-							+ medios + "\nSuma de los numeros grandes " + grandes);
+		System.out.println("Numeros bajos: " + bajos + "\nNumeros medios: " + medios + "\nNumeros grandes " + grandes);
 		
 	}
 }
