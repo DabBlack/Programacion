8356f8c02fbbc808861a7c9760168c971e9f17e7
DabBlack <davidalgar2@gmail.com>
2021-11-03 14:56:24 +0100

    Actualizacion Ejercicios

--- src/capitulo05_bloque03/Ejercicio01.java
+++ src/capitulo05_bloque03/Ejercicio01.java
@@ -5,6 +5,8 @@ public class Ejercicio01 {
 	public static void main(String[] args) {
 		
 		//Declaracion del array
+		int auxiliar = 0;
+		boolean hayIntercambios = true;
 		int numeros[] = new int[150];
 		
 		//Inicializacion de los valores de array aleatoriamente
@@ -21,7 +23,32 @@ public class Ejercicio01 {
 		
 		System.out.println("\n" + "\n" + "Array ordenado:" + "\n");
 		
+		do {
+			//Para saber si hay un intercambio de primeras ponemos nuestra banderita abajo, es decir el booleano en false
+			hayIntercambios = false;
+			for (int i = 0; i < numeros.length - 1; i++) {
+				//La condicion para el intercambio es que el valor de num sea mayor que el valor del num posterior, si lo es alzamos banderita,
+				//es decir, se pone la variable booleana en true
+				if (numeros[i] > numeros[i + 1]) {
+					hayIntercambios = true;
+					//A la variable auxiliar se le asigna el valor del num posterior, mientras que al num posterior le asignamos el valor de num
+					auxiliar = numeros[i + 1];
+					numeros[i + 1] = numeros[i];
+					//A num le asignamos el valor de la variable auxiliar para guardar su valor
+					numeros[i] = auxiliar;
+				}
+				
+			}
+			//Una vez que ha leido la linea del Array y no habia ningun intercambio, nuestra banderita se bajaria, es decir la variable booleana
+			//se pondria en false, por lo tanto no cumpliria la condicion del do while y pasariamos a imprimir el Array ordenado
+			
+		} while(hayIntercambios == true);
 
+		//Para imprimir el Array ordenado utilizamos otro for fuera del bucle while, para asi recorrer el Array ya ordenado
+		for(int i = 0; i < numeros.length; i++ ) {
+			System.out.print(numeros[i] + " ");
+		}
+		
 	}
 
 }
---
+++ src/capitulo05_bloque03/Ejercicio02.java
@@ -0,0 +1,57 @@
+package capitulo05_bloque03;
+
+public class Ejercicio02 {
+
+	public static void main(String[] args) {
+		
+		//Declaracion del array
+		int auxiliar = 0, posicionMenor;
+		int numeros[] = new int[150];
+		
+		//Inicializacion de los valores de array aleatoriamente
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = (int) Math.round(Math.random() * 1000);
+		}
+		
+		System.out.println("Array sin ordenar:" + "\n");
+		
+		//Recorrido del array para imprimir sus valores en pantalla
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.print(numeros[i] + " ");
+		}
+		
+		System.out.println("\n" + "\n" + "Array ordenado:" + "\n");
+		
+		//Se inicia un bucle para recorrer el array
+		for (int i = 0; i < numeros.length; i++) {
+			//Se le asigna a la variable posicionMenor el valor de la posicion que corresponde en ese momento a la i
+			posicionMenor = i;
+			
+			//Este bucle parte de la siguiente posicion de i
+			for (int j = i + 1; j < numeros.length; j++) {
+				//Si el valor de la posicion j es menor que la posicion de posicionMenor, como posicionMenor = i
+				//equivale a la posicion de la izquierda de la j, lo que hace es comparar el de la izquierda con
+				//el de la derecha y cambia el valor si es menor
+				if (numeros[j] < numeros[posicionMenor]) {
+					posicionMenor = j;
+				}
+			}
+			
+			//Se le asigna a la variable
+			auxiliar = numeros[posicionMenor];
+			for (int j = posicionMenor; j > i; j--) {
+				numeros[j] = numeros[j - 1];
+			}
+			
+			numeros[i] = auxiliar;
+			
+		}
+		
+		//Recorrido del array para imprimir sus valores en pantalla
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.print(numeros[i] + " ");
+		}
+		
+	}
+
+}
