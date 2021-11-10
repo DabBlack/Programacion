52ba93dcfcd160b2d529e6b0d2ee3e214e462edf
DabBlack <davidalgar2@gmail.com>
2021-11-10 14:35:01 +0100

    Actualizacion Ejercicios

--- src/capitulo05_bloque05/Ejercicio01_LongitudArrayParImpar.java
+++ src/capitulo05_bloque05/Ejercicio01_LongitudArrayParImpar.java
@@ -6,38 +6,43 @@ public class Ejercicio01_LongitudArrayParImpar {
 
 	public static void main(String[] args) {
 		
+		//Se declara un array de longitud al azar
 		int array[] = new int[Utils.obtenerNumeroAzar(0, 50)];
 				
 		System.out.println("Array: " + "\n");
 		
-		inicializarArray(array);
+		//Se llaman a los siguientes metodos, que inicializan el array con valores al azar y lo muestra
+		inicializarArrayAlAzar(array);
 		mostrarArray(array);
 		
 		System.out.println("\n" + "\n" + "La longitud del array es: " + array.length);
 		System.out.println("\n" + "La posicion media del array es: " + (array.length / 2));
 		
+		//Este metodo hace que se analice el array, viendo si la longitud es par o impar
 		analizarArray(array);
 		
+		//Longitud impar
 		if (array.length % 2 == 1) {
 			System.out.println("\n" + "El valor de la posicion media del array es: ");
 			System.out.println(analizarArray(array));
 		}
 		
+		//Longitud par
 		else {
 			System.out.println("\n" + "Al ser par se devuelve:");
 			System.out.println(analizarArray(array));
 		}
 	}
 
-	
-	public static void inicializarArray (int array[]) {
+	//Metodo para inicializar el array asisgandole valores al azar entre 0 y 10
+	public static void inicializarArrayAlAzar (int array[]) {
 		for (int i = 0; i < array.length; i++) {
 			array[i] = Utils.obtenerNumeroAzar(0, 100);
 		}
 		
 	}
 	
-	
+	//Metodo para mostrar el array
 	public static void mostrarArray (int array[]) {
 		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i] + " ");
@@ -45,9 +50,9 @@ public class Ejercicio01_LongitudArrayParImpar {
 		
 	}
 	
-	
+	//Metodo para analizar el array, comprobando es la longitud es par o impar, devolviendo un valor en cada caso
+	//Valor que se usara mas tarde
 	public static int analizarArray (int array[]) {
-		
 		if (array.length % 2 == 1) {
 			return array[array.length / 2];
 		}
--- src/capitulo05_bloque05/Ejercicio02_Notas.java
+++ src/capitulo05_bloque05/Ejercicio02_Notas.java
@@ -9,18 +9,19 @@ public class Ejercicio02_Notas {
 		int array[] = new int[20];
 
 		System.out.println("Notas: " + "\n");
-		
-		inicializarArray(array);
+	
+		//Este metodo inicializa el array al azar
+		inicializarArrayAlAzar(array);
+		//Este metodo muestra el array
 		mostrarArray(array);
-		
-		System.out.println("\n" + "\n" + "El porcentaje de suspensos es: ");
-		System.out.println("\n" + "El porcentaje de aprobados es: ");
-		
+		//Este metodo analiza el array para contar los valores mayores y menores que 5
+		//Una vez tengamos los contadores en el mismo syso haremos las operaciones necesarias
 		analizarArray(array);
 		
 	}
 
-	public static void inicializarArray (int array[]) {
+	public static void inicializarArrayAlAzar (int array[]) {
+		//Inicializamos el array asisgandole valores al azar entre 0 y 10
 		for (int i = 0; i < array.length; i++) {
 			array[i] = Utils.obtenerNumeroAzar(0, 10);
 		}
@@ -29,6 +30,7 @@ public class Ejercicio02_Notas {
 	
 	
 	public static void mostrarArray (int array[]) {
+		//Mostramos el array
 		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i] + " ");
 		}
@@ -36,18 +38,25 @@ public class Ejercicio02_Notas {
 	}
 	
 	
-	public static int analizarArray (int array[]) {
-		int aprobado = 0, suspenso = 0;
+	public static void analizarArray (int array[]) {
+		int aprobados = 0, suspensos = 0;
+		
+		//Diferenciamos los valores mayores y menores de 5, haciendo un incremento simple a la variable para usarla de contador
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] >= 5){
-				array[i] = aprobado;
+				aprobados++;
 			}
+			
 			else {
-				array[i] = suspenso;
+				suspensos++;
 			}
+			
 		}
 		
-		return 0;
+		//Mostramos en pantalla los valores ademas de hacer los calculos necesarios
+		System.out.println("\n" + "\n" + "El porcentaje de aprobados es: " + (float) (aprobados * 100) / array.length + "%");
+		System.out.println("\n" + "El porcentaje de suspensos es: " + (float) (suspensos * 100) / array.length + "%");
+		
 		
 	}
 	
