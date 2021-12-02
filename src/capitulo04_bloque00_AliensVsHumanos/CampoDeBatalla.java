eb762ef042c91554c79f4de079c1d22a4e04860f
DabBlack <davidalgar2@gmail.com>
2021-12-02 15:01:02 +0100

    Actualizacion Ejercicios

--- src/capitulo04_bloque00_AliensVsHumanos/CampoDeBatalla.java
+++ src/capitulo04_bloque00_AliensVsHumanos/CampoDeBatalla.java
@@ -4,4 +4,16 @@ public class CampoDeBatalla {
 	
 	private Humano arrayHumanos[] = new Humano[5];
 	private Alien arrayAliens[] = new Alien[5];
+	
+	public CampoDeBatalla() {
+		for (int i = 0; i < arrayAliens.length; i++) {
+			arrayAliens[i] = new Alien();
+		}
+		
+		Alien ultimoAlien = arrayAliens[arrayAliens.length - 1];
+		ultimoAlien.setVida(ultimoAlien.getVida() * 10);
+	}
+	
+	
+	
 }
