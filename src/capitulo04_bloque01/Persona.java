6d44de4bd8ad12e6134d50abfebb3fec7e44a41f
david <david@DESKTOP-QJ0G38O>
2021-12-12 23:12:14 +0100

    Actualizacion Ejercicios

---
+++ src/capitulo04_bloque01/Persona.java
@@ -0,0 +1,119 @@
+package capitulo04_bloque01;
+
+public class Persona {
+	private String nombre, apellido, dni, direccion, telefono;
+	
+	/**
+	 * 
+	 */
+	public Persona() {
+		super();
+	}
+
+
+	/**
+	 * @param nombre
+	 * @param apellidos
+	 * @param dni
+	 * @param direccion
+	 * @param telefono
+	 */
+	public Persona(String nombre, String apellido, String dni, String direccion, String telefono) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.direccion = direccion;
+		this.telefono = telefono;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", direccion=" + direccion
+				+ ", telefono=" + telefono + "]";
+	}
+
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	/**
+	 * @return the apellidos
+	 */
+	public String getApellido() {
+		return apellido;
+	}
+
+
+	/**
+	 * @param apellidos the apellidos to set
+	 */
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+	/**
+	 * @return the dni
+	 */
+	public String getDni() {
+		return dni;
+	}
+
+
+	/**
+	 * @param dni the dni to set
+	 */
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+
+	/**
+	 * @return the direccion
+	 */
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+	/**
+	 * @param direccion the direccion to set
+	 */
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+	/**
+	 * @return the telefono
+	 */
+	public String getTelefono() {
+		return telefono;
+	}
+
+
+	/**
+	 * @param telefono the telefono to set
+	 */
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	
+}
+
---
+++ src/capitulo04_bloque01/Principal.java
@@ -0,0 +1,117 @@
+package capitulo04_bloque01;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Persona arrayPersona[] = new Persona[3];
+		
+		boolean numPersonas = false; // Este boolean comprueba el número de personas que se han creado
+		boolean quiereSalirDelPrograma = false;// Este boolean comprueba si el usuario quiere salir del programa
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//Necesitamos un bucle do para entrar minimamente una vez y salir si hay tres personas creadas o si el usuario a elegido salir
+		do {
+			System.out.println("¿Que necesita hacer?" + "\n" + "\n" + "Menu" + "\n" + "-0. Salir" + "\n"
+					+ "-1. Crear una persona (Máximo 3)" + "\n" + "-2. Consultar personas introducidas");
+			int opcionElegidaPorElUsuario = sc.nextInt();
+			
+			switch (opcionElegidaPorElUsuario) {
+			case 0: {
+				quiereSalirDelPrograma = true; //Se validan las variables booleanas para que el programa termine
+				numPersonas = true; //Se validan las variables booleanas para que el programa termine
+				System.out.println("Has salido del programa");
+				break;
+			}
+			case 1: {
+				//Creo una persona llamando a un metodo que devuelve un booleano (controlando si hay 3 personas creadas)
+				numPersonas = crearPersona(arrayPersona); 
+				break;
+			}
+			case 2: {
+				//Visualizo las personas creadas anteriormente llamando a un método  que muestra en pantalla la información de cada una
+				visualizarPersona(arrayPersona);
+				break;
+			}
+			default:
+				//Si el usuario elije algo distinto aparecera este mensaje en pantalla
+				System.out.println("Selección inválida");
+			}
+		} while (!numPersonas || !quiereSalirDelPrograma);
+	}
+
+	public static boolean crearPersona(Persona[] arrayPersona) {
+		boolean personaCreada = false; // Esta variable booleana controla si se ha creado una persona en el método
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Creacion de una persona");
+		
+		for (int i = 0; i < arrayPersona.length; i++) {
+			personaCreada = false;
+			
+			// Busca un hueco (null) en el array y crea una persona nueva
+			if (arrayPersona[i] == null) {
+				System.out.println("Introduce su nombre:");
+				String nombre = sc.next();
+				
+				System.out.println("Introduce su apellido:");
+				String apellidos = sc.next();
+				
+				System.out.println("Introduce su DNI:");
+				String dni = sc.next();
+				
+				System.out.println("Introduce su direccion:");
+				String direccion = sc.next();
+				
+				System.out.println("Introduce su telefono:");
+				String telefono = sc.next();
+				
+				//Creacion de una persona
+				arrayPersona[i] = new Persona(nombre, apellidos, dni, direccion, telefono);
+				personaCreada = true;
+			}
+			
+			//Si hay 3 personas devuelve true
+			if (i == arrayPersona.length - 1) {
+				return true;
+			}
+			
+			//Pregunta al usuario si quiere seguir creando personas tras crear una
+			else  if (personaCreada) {
+				boolean opcionValida = false;
+				
+				while (!opcionValida) {
+					System.out.println("¿Quieres crear otra persona? Si/No");
+					String aux = sc.next();
+					
+					if (aux.contentEquals("Si")) {
+						System.out.println("Vas a crear otra persona");
+						opcionValida = true;
+					}
+					else if (aux.contentEquals("No")) {
+						return false; //Aqui detenemos el metodo y devolvemos false
+					}
+				}
+				
+			}
+		}
+		return false;
+	}
+
+	public static void visualizarPersona(Persona[] arrayPersona) {
+		for (int i = 0; i < arrayPersona.length; i++) {
+			//Si se cumple la condicion el bucle imprime las personas en orden
+			if (arrayPersona[i] != null) {
+				System.out.println(arrayPersona[i].toString() + "\n");
+			}
+			//Si no se cumple la condicion el metodo se detendra con el break
+			else {
+				 break;
+			}
+		}
+	}
+
+
+}
