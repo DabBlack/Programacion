eaa2d2ef881dd9157bc9f52ea2e9912c99177e16
david <david@DESKTOP-QJ0G38O>
2022-01-09 22:52:49 +0100

    Actualizacion Ejercicios

--- src/capitulo05_bloque08/Ejercicio02_listaString.java
+++ src/capitulo05_bloque08/Ejercicio02_listaString.java
@@ -7,48 +7,61 @@ import java.util.Scanner;
 public class Ejercicio02_listaString {
 
 	public static void main(String[] args) {
+		
+		// Declaracion de variables
 		String opcion;
 
 		List<String> texto = new ArrayList<String>();
 		List<String> portapapeles = new ArrayList<String>();
 
+		// Este bucle repite el switch hasta que se introduzca el valor "a"
 		do {
-			System.out.println("\nMENU:\n" + "a) Abandonar el programa.\n" + "b) Agregar una linea al texto.\n" + "c) Insertar una linea en cualquier posicion del texto.\n" + 
+			System.out.println("\nMENU:\n" + "\na) Abandonar el programa.\n" + "b) Agregar una linea al texto.\n" + "c) Insertar una linea en una determinada posicion de la lista.\n" + 
 					"d) Editar una linea.\n" + "e) Borrar una linea.\n" + "f) Cortar un conjunto de lineas.\n" + "g) Pegar un conjunto de lineas cortadas en una determinada posicion.\n" + 
-					"h) Imprimir el fichero.");
+					"h) Mostrar la lista de texto.");
 
 			Scanner sc = new Scanner(System.in);
 			opcion = sc.next();
 
 			switch (opcion) {
 			case "a":
+				System.out.println("El programa ha terminado.");
 				break;
 
 			case "b":
-				nuevaLinea(texto);
+				// Añadir una nueva linea de texto introducida por el usuario
+				nuevaLineaDeTexto(texto);
 				break;
 
 			case "c":
-				nuevaLineaEnPosicion(texto);
+				// Añadir una nueva linea de texto introducida por el usuario en una determinada posicion de la lista
+				// introducida por el usuario
+				nuevaLineaEnUnaPosicion(texto);
 				break;
 
 			case "d":
-				editarLinea(texto);
+				// Editar una linea de texto indicada por el usuario
+				editarUnaLinea(texto);
 				break;
 
 			case "e":
+				// Eliminar una linea de texto indicada por el usuario
 				borrarLinea(texto);
 				break;
 
 			case "f":
+				// Cortar (elimina) un conjunto de lineas indicadas por el usuario y las guarda en el portapapeles
 				cortarLineas(texto, portapapeles);
 				break;
 
 			case "g":
+				// Pegar en una determinada posicion de la lista indicada por el usuario las lineas cortadas
+				// y guardadas con el metodo "cortarLineas" en el portapapeles
 				pegarLineas(texto, portapapeles);
 				break;
 
 			case "h":
+				// Mostrar la lista con el texto
 				mostrarTexto(texto);
 				break;
 			}
@@ -56,40 +69,61 @@ public class Ejercicio02_listaString {
 		} while (!(opcion.equals("a")));
 	}
 
-	public static void nuevaLinea(List<String> l) {
-		System.out.println("Introduce el texto que quieras añadir a la línea: ");
+	
+	/**
+	 * Metodo que añade una nueva linea de texto introducida por el usuario
+	 * @param l
+	 */
+	public static void nuevaLineaDeTexto(List<String> l) {
+		System.out.println("Introduzca el texto que desea añadir a la linea: ");
 		Scanner sc = new Scanner(System.in);
 		String linea = sc.nextLine();
 
 		l.add(linea);
 	}
 
-	public static void mostrarTexto(List<String> l) {
+	
+	/**
+	 * Metodo que muestra el texto que hay en la lista
+	 * @param l
+	 */
+	public static void mostrarTexto (List<String> l) {
 		for (int i = 0; i < l.size(); i++) {
 			System.out.println(i + ".\t" + l.get(i) + " ");
 		}
 	}
 
-	public static void nuevaLineaEnPosicion(List<String> l) {
-		System.out.println("Introduce el texto que quieras añadir a la línea: ");
+	
+	/**
+	 * Metodo que añade una nueva linea de texto introducida por el usuario en una determinada posicion de la lista
+	 * introducida por el usuario
+	 * @param l
+	 */
+	public static void nuevaLineaEnUnaPosicion (List<String> l) {
+		System.out.println("Introduzca el texto que desea añadir a la linea: ");
 		Scanner sc = new Scanner(System.in);
 		String linea = sc.nextLine();
 
-		System.out.println("¿En qué posición quieres añadir la línea?");
+		System.out.println("¿En que posicion desea añadir la linea que acaba de escribir?");
 		sc = new Scanner(System.in);
 		int indice = sc.nextInt();
 
 		l.add(indice, linea);
 	}
 
-	public static void editarLinea(List<String> l) {
-		System.out.println("¿Qué línea quieres reescribir?");
+	
+	/**
+	 * Metodo que edita una linea indicada por el usuario
+	 * @param l
+	 */
+	public static void editarUnaLinea (List<String> l) {
+		System.out.println("¿Que linea desea reescribir?");
 		Scanner sc = new Scanner(System.in);
 		int indice = sc.nextInt();
 
 		l.remove(indice);
 
-		System.out.println("Introduce el texto que quieras añadir a la línea: ");
+		System.out.println("Introduzca el texto que desea añadir a la linea: ");
 		sc = new Scanner(System.in);
 		String linea = sc.nextLine();
 
@@ -97,8 +131,13 @@ public class Ejercicio02_listaString {
 
 	}
 
-	public static void borrarLinea(List<String> l) {
-		System.out.println("¿Qué línea quieres borrar?");
+	
+	/**
+	 * Metodo que elimina una linea indicada por el usuario
+	 * @param l
+	 */
+	public static void borrarLinea (List<String> l) {
+		System.out.println("¿Que linea desea borrar?");
 		Scanner sc = new Scanner(System.in);
 		int indice = sc.nextInt();
 
@@ -106,12 +145,18 @@ public class Ejercicio02_listaString {
 
 	}
 
-	public static void cortarLineas(List<String> l, List<String> cortar) {
-		System.out.println("¿Desde qué línea quieres cortar?");
+	
+	/**
+	 * Metodo que corta (elimina) un conjunto de lineas indicadas por el usuario y las guarda en el portapapeles
+	 * @param l
+	 * @param cortar
+	 */
+	public static void cortarLineas (List<String> l, List<String> cortar) {
+		System.out.println("¿Desde que linea desea cortar?");
 		Scanner sc = new Scanner(System.in);
 		int min = sc.nextInt();
 
-		System.out.println("¿Hasta qué línea quieres cortar?");
+		System.out.println("¿Hasta que linea desea cortar?");
 		sc = new Scanner(System.in);
 		int max = sc.nextInt();
 
@@ -121,21 +166,27 @@ public class Ejercicio02_listaString {
 		}
 	}
 
-	public static void pegarLineas(List<String> l, List<String> pegar) {
-		System.out.println("Las líneas que hay en el portapapeles son las siguientes:");
+	
+	/**
+	 * Este metodo pega en una determinada posicion de la lista indicada por el usuario las lineas cortadas 
+	 * y guardadas con el metodo "cortarLineas" en el portapapeles
+	 * @param l
+	 * @param pegar
+	 */
+	public static void pegarLineas (List<String> l, List<String> pegar) {
+		System.out.println("Las lineas almacenadas en el portapapeles son:");
 
 		for (int i = 0; i < pegar.size(); i++) {
 			System.out.println(pegar.get(i));
 		}
 
-		System.out.println("¿En qué posición quieres pegarlas?:");
+		System.out.println("¿En que posicion desea pegar las lineas?:");
 		Scanner sc = new Scanner(System.in);
 		int indice = sc.nextInt();
 
 		l.addAll(indice, pegar);
 
 		pegar.clear();
-
 	}
 
 }
