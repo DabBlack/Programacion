eb65465406ed340ed9f6007fc8cb27fd4636fbb1
DabBlack <davidalgar2@gmail.com>
2022-03-31 14:48:13 +0200

    Actualizacion Ejercicios

---
+++ src/capitulo08_Entorno_Grafico_Swing_Fabricante/CRUD_Fabricante.java
@@ -0,0 +1,209 @@
+package capitulo08_Entorno_Grafico_Swing_Fabricante;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.Font;
+import javax.swing.JToggleButton;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class CRUD_Fabricante extends JPanel {
+
+	private JFrame frame;
+	private JTextField jtfId;
+	private JTextField jtfCif;
+	private JTextField jtfNombre;
+	private JLabel lblNewLabel_4;
+	private JPanel panel;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+	private JButton btnNewButton_2;
+	private JButton btnNewButton_3;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CRUD_Fabricante window = new CRUD_Fabricante();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public CRUD_Fabricante() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+		
+		JLabel lblNewLabel_1 = new JLabel("Gestión de fabricantes");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.gridwidth = 2;
+		gbc_lblNewLabel_1.insets = new Insets(15, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 0;
+		frame.getContentPane().add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		JLabel lblNewLabel = new JLabel("Id:");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 1;
+		frame.getContentPane().add(lblNewLabel, gbc_lblNewLabel);
+		
+		jtfId = new JTextField();
+		jtfId.setEnabled(false);
+		GridBagConstraints gbc_jtfId = new GridBagConstraints();
+		gbc_jtfId.insets = new Insets(0, 0, 5, 5);
+		gbc_jtfId.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jtfId.gridx = 1;
+		gbc_jtfId.gridy = 1;
+		frame.getContentPane().add(jtfId, gbc_jtfId);
+		jtfId.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("CIF:");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 0;
+		gbc_lblNewLabel_2.gridy = 2;
+		frame.getContentPane().add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		jtfCif = new JTextField();
+		GridBagConstraints gbc_jtfCif = new GridBagConstraints();
+		gbc_jtfCif.insets = new Insets(0, 0, 5, 5);
+		gbc_jtfCif.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jtfCif.gridx = 1;
+		gbc_jtfCif.gridy = 2;
+		frame.getContentPane().add(jtfCif, gbc_jtfCif);
+		jtfCif.setColumns(10);
+		
+		lblNewLabel_4 = new JLabel(" ");
+		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 15);
+		gbc_lblNewLabel_4.gridx = 2;
+		gbc_lblNewLabel_4.gridy = 2;
+		frame.getContentPane().add(lblNewLabel_4, gbc_lblNewLabel_4);
+		
+		JLabel lblNewLabel_3 = new JLabel("Nombre:");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 0;
+		gbc_lblNewLabel_3.gridy = 3;
+		frame.getContentPane().add(lblNewLabel_3, gbc_lblNewLabel_3);
+		
+		jtfNombre = new JTextField();
+		GridBagConstraints gbc_jtfNombre = new GridBagConstraints();
+		gbc_jtfNombre.insets = new Insets(0, 0, 5, 5);
+		gbc_jtfNombre.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jtfNombre.gridx = 1;
+		gbc_jtfNombre.gridy = 3;
+		frame.getContentPane().add(jtfNombre, gbc_jtfNombre);
+		jtfNombre.setColumns(10);
+		
+		panel = new JPanel();
+		panel.setBackground(Color.GRAY);
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.gridwidth = 3;
+		gbc_panel.insets = new Insets(0, 0, 0, 5);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 4;
+		frame.getContentPane().add(panel, gbc_panel);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		btnNewButton = new JButton("<<");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mostrarPrimerFabricante();
+			}
+		});
+		panel.add(btnNewButton);
+		
+		btnNewButton_1 = new JButton("<");
+		panel.add(btnNewButton_1);
+		
+		btnNewButton_2 = new JButton(">");
+		panel.add(btnNewButton_2);
+		
+		btnNewButton_3 = new JButton(">>");
+		panel.add(btnNewButton_3);
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	/**
+	 * 
+	 */
+	public void mostrarPrimerFabricante() {
+		try {
+			// Para poder ejecutar una consulta necesitamos utilizar un objeto de tipo Statement
+			Statement s = (Statement) ConnectionManager.getConexion().createStatement(); 
+			
+			// La ejecución de la consulta se realiza a través del objeto Statement y se recibe en forma de objeto
+			// de tipo ResultSet, que puede ser navegado para descubrir todos los registros obtenidos por la consulta
+			ResultSet rs = s.executeQuery ("select * from fabricante order by id limit 1");
+		   
+			// Navegación del objeto ResultSet
+			if (rs.next()) { 
+				this.jtfId.setText(rs.getString("id"));
+				this.jtfCif.setText(rs.getString("cif"));
+				this.jtfNombre.setText(rs.getString("nombre"));
+			}
+			// Cierre de los elementos
+			rs.close();
+			s.close();
+		}
+		catch (SQLException ex) {
+			System.out.println("Error en la ejecución SQL: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+
+
+}
